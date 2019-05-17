ffab31681e59556e29afbc1f50208ba8466ab319
shahid37 <happyshahid37@gmail.com>
2019-05-18 01:43:13 +0500

    binary tree example

---
+++ tree_problem.py
@@ -0,0 +1,58 @@
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def print_tree(self):
+        print(self.data)
+
+
+root = Node(10)
+
+
+root.print_tree()
+
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+# Print the tree
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        print(self.data),
+        if self.right:
+            self.right.print_tree()
+
+
+# Use the insert method to add nodes
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(30)
+root.insert(35)
+root.insert(3)
+root.insert(39)
+root.print_tree()
